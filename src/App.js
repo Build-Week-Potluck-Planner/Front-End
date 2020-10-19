b1bec84614bff5421fe4d76aa362049eea328f23
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2020-10-19 14:18:50 -0400

    FrontEnd - file structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Potluck Planner, Track Team #8 </h1>
+      <h2>David Chang, Unit 2</h2>
+      <br></br>
+      <h2>Elijah Elliot, Unit 2</h2>
+      <br></br>
+      <h2>Christopher Stankiewicz, Unit 3</h2>
+      <h2>Tzong-Lian Tsay, Unit 3</h2>
     </div>
   );
 }
