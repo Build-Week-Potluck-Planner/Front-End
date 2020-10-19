dbdc0a9e864ea0a17a713049c98619ea271d0846
CStank89 <C.Stank89@gmail.com>
2020-10-19 17:00:00 -0400

    just added the router and private route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import "./App.css";
+import Private from "./utils/PrivateRoute";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AxiosWithAuto from "./utils/AxiosWithAuth";
 
 function App() {
   return (
     <div className="App">
-      <h1>Potluck Planner, Track Team #8 </h1>
-      <h2>David Chang, Unit 2</h2>
-      <br></br>
-      <h2>Elijah Elliot, Unit 2</h2>
-      <br></br>
-      <h2>Christopher Stankiewicz, Unit 3</h2>
-      <br></br>
-      <h2>Tzong-Lian Tsay, Unit 3</h2>
-      <br></br>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <h1>Plan A Potluck</h1>
+            <Route exact path="/" component={} />
+          </header>
+          <div className="">
+            <Private path="/protected" component={} />
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }
