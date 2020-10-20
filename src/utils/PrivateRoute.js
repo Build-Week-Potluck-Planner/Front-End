5ef51e711d5b02a35ad3e66f5ce9767881f7f3ce
CStank89 <C.Stank89@gmail.com>
2020-10-20 11:31:28 -0400

    save

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Redirect, Redirect } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import OrganizerCard from "../components/OrganizerCard";
 
 const Private = ({ component: OrganizerCard, ...props }) => {
