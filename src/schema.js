1a4cd6d089e8fedf7300639f9641ce3a101a7b2f
david chang <bjchang@Davids-MacBook-Air.local>
2020-10-21 10:31:35 -0700

    Changed merge conflicts regarding package-lock.json and organizer.js

--- src/schema.js
+++ src/schema.js
@@ -5,12 +5,12 @@ export default yup.object().shape({
         .string()
         .required('Please enter a name for the potluck event.')
         .min(6, 'Name of the event must be at least six characters'),
-    date: yup
-        .number()
-        .required('Please enter a date for the potluck event.'),
-    time: yup
-        .number()
-        .required('Please enter a time for the potluck event.'),
+    // date: yup
+    //     .number()
+    //     .required('Please enter a date for the potluck event.'),
+    // time: yup
+    //     .number()
+    //     .required('Please enter a time for the potluck event.'),
     description: yup
         .string()
         .required('Please enter a description for the potluck event.'),
