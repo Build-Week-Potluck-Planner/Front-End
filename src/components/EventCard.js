6db69666e1d30638670f35fdfa714bd4f86acd82
CStank89 <C.Stank89@gmail.com>
2020-10-21 14:26:10 -0400

    switched to material ui and started styling the cards a little more

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,27 +1,69 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
-export const EventCard = ({ props }) => {
-  console.log(props);
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
+function EventCard({ props }) {
+  const classes = useStyles();
+
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title> {props.event_name} </Card.Title>
-        <Card.Text>
-          <p>{props.description}</p>
-          <p>{props.event_id}</p>
-          <p>{props.date}</p>
-          <p>{props.time}</p>
-          <p>
-            {props.state}
-            {props.address}
-          </p>
-        </Card.Text>
-        <Button variant="Primary">Edit</Button>
-        <Button variant="Warning">Delete</Button>
-      </Card.Body>
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.event_name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.description}
+            <p>
+              {props.event_id} <br></br>
+              {props.date} <br></br>
+              {props.time} <br></br>
+              {props.state} <br></br>
+              {props.address} <br></br>
+            </p>{" "}
+            <br></br>
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Edit
+        </Button>
+        <Button size="small" color="primary">
+          Delete
+        </Button>
+      </CardActions>
     </Card>
   );
-};
+}
+
+export default EventCard;
+// {props.event_name}
+// <p>{props.description}</p>
+// <p>{props.event_id}</p>
+// <p>{props.date}</p>
+// <p>{props.time}</p>
+// <p>
+//   {props.state}
+//   {props.address}
+// </p>
