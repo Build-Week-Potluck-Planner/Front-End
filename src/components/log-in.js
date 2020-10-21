726727b4106b64fb76ba29efbc7da02be536ed39
Tzong-Lian-Tsay <tzong-lian-tsay@lambdastudents.com>
2020-10-21 13:29:18 -0400

    added login

--- src/components/log-in.js
+++ src/components/log-in.js
@@ -1,61 +1,66 @@
-import React, { useState } from 'react';
-import * as yup from 'yup';
-import axios from 'axios'
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import * as yup from "yup";
+import axios from "axios";
 
-export default function Login () {
+export default function Login() {
+  const { push } = useHistory();
 
-    const [loginData, setLoginData] = useState({
-        username: '',
-        password: '',
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const onChange = (event) => {
+    setLoginData({
+      ...loginData,
+      [event.target.name]: event.target.value,
     });
+  };
 
-    const onChange = event => {
-        setLoginData({
-            ...loginData, 
-            [event.target.name]: event.target.value,
-        })
-    }
+  const onSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("https://potluck-planner-bw.herokuapp.com/users/login", loginData)
+      .then((result) => {
+        console.log(result.data);
+        localStorage.setItem("token", result.data.token);
+        push("/Home");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    const onSubmit = event => {
-        event.preventDefault();
-        axios.post('https://potluck-planner-bw.herokuapp.com/users/login', user)
-        .then(result => {
-            console.log(result.data);
-            localStorage.setItem('token', result.data.token);
-            push('/Home');
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    };
+  return (
+    <div className="logIn inputs">
+      <form onSubmit={onSubmit}>
+        <label>
+          Username
+          <input
+            type="text"
+            name="username"
+            onChange={onChange}
+            placeholder="Enter Username"
+            maxLength="20"
+          />
+        </label>
 
-    return (
-        <div className='logIn inputs'>
-            <form onSubmit={onSubmit}>
-                <label>Username
-                    <input 
-                    type='text' 
-                    name='username' 
-                    onChange={onChange} 
-                    placeholder='Enter Username' 
-                    maxLength='20'
-                    />
-                </label>
+        <label>
+          Password
+          <input
+            type="text"
+            name="password"
+            onChange={onChange}
+            placeholder="Enter Password"
+            maxLength="12"
+          />
+        </label>
 
-                <label>Password
-                    <input 
-                    type='text'
-                    name='password'
-                    onChange={onChange}
-                    placeholder='Enter Password'
-                    maxLength='12'
-                    />
-                </label>
-
-                <div className='enter'>
-                    <button>Enter</button>
-                </div>
-            </form>
+        <div className="enter">
+          <button>Enter</button>
         </div>
-    )
+      </form>
+    </div>
+  );
 }
