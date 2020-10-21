4dd702bb85bf4229d72c60374fcc2250d46828ac
david chang <bjchang@Davids-MacBook-Air.local>
2020-10-21 15:20:10 -0700

    Changes to schema

--- src/schema.js
+++ src/schema.js
@@ -5,15 +5,16 @@ export default yup.object().shape({
         .string()
         .required('Please enter a name for the potluck event.')
         .min(6, 'Name of the event must be at least six characters'),
-    // date: yup
-    //     .number()
-    //     .required('Please enter a date for the potluck event.'),
-    // time: yup
-    //     .number()
-    //     .required('Please enter a time for the potluck event.'),
+    date: yup
+        .date()
+        .required('Please enter a date for the potluck event.'),
+    time: yup
+        .number()
+        .required('Please enter a time for the potluck event.'),
     description: yup
         .string()
-        .required('Please enter a description for the potluck event.'),
+        .required('Please enter a description for the potluck event.')
+        .min(5, 'Please provide a description for the event.'),
     address: yup
         .string()
         .required('Please enter an address for the potluck event.'),
