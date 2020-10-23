99a4a061d39a34a638fdde2b3373e64c04965540
Eelli002 <elijah_elliott@hotmail.com>
2020-10-22 22:36:34 -0700

    Finished Sign Up and Log In Styling

--- src/components/sign-up.js
+++ src/components/sign-up.js
@@ -1,17 +1,18 @@
 import React, { useEffect, useState } from "react";
 import * as yup from "yup";
-import { useHistory, Route, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import { authorization } from "../utils/AxiosWithAuth";
 import styled from 'styled-components'
 
 
 const StyledCard = styled.div `
-  border: 2px solid red;
   background-color: #ffffff;
+  border-radius: 10px;
+  padding-left: 3%;
+  padding-right: 3%;
 `
 
 const Form = styled.form `
-  // border: 2px solid green;
   margin: 2%;
   display: flex;
   flex-direction: column;
@@ -22,14 +23,14 @@ const Form = styled.form `
 
 const SubmitButton = styled.button `
   margin: 5%;
+  border-radius: 15px;
 `
 
 const LoginButton = styled.button `
-
+  border-radius: 15px;
 `
 
 const Label = styled.label `
-  // border: 2px solid green;
   padding-top: 5%;
   font-size: 25px;
   font-weight: normal;
@@ -40,8 +41,10 @@ const P = styled.p `
 `
 
 const Input = styled.input `
-  padding: 3%;
-  // border-radius: 15%;
+  padding: 5%;
+  border-radius: 5px;
+  border: 1px solid gray;
+  font-weight: bold;
 `
 
 const schema = yup.object().shape({
@@ -109,51 +112,51 @@ export default function Signup() {
 
   return (
     <StyledCard>
-      <Form onSubmit={onSubmit}> Create Your Account!
+      <Form onSubmit={onSubmit}> Create Your Account
         <Label>
-          Username
+          
           <Input
             type="text"
             name="username"
             value={signUpData.username}
             onChange={onChange}
-            placeholder="Enter Username"
+            placeholder="Username"
             maxLength="20"
           />
         </Label>
 
         <Label>
-          Password
+          
           <Input
             type="text"
             name="password"
             value={signUpData.password}
             onChange={onChange}
-            placeholder="Enter Password"
+            placeholder="Password"
             maxLength="12"
           />
         </Label>
 
         <Label>
-          Full Name
+          
           <Input
             type="text"
             name="full_name"
             value={signUpData.full_name}
             onChange={onChange}
-            placeholder="Enter Your Full Name"
+            placeholder="Full Name"
             maxLength="25"
           />
         </Label>
 
         <Label>
-          Email
+          
           <Input
             type="email"
             name="email"
             value={signUpData.email}
             onChange={onChange}
-            placeholder="Enter Your Email"
+            placeholder="Email"
             maxLength="35"
           />
         </Label>
@@ -161,7 +164,7 @@ export default function Signup() {
         <div>
           <SubmitButton disabled={disabled}>Register Now!</SubmitButton>
           <P>Already Have an Account? Log In Here!</P>
-          <button onClick={() => push("/login")}>Login</button>
+          <LoginButton onClick={() => push("/login")}>Login</LoginButton>
         </div>
       </Form>
     </StyledCard>
